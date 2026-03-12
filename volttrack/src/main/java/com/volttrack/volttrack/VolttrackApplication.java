0f38d3e393c91ea37eb0965f9696de903a349bca
MGhiremath0281 <muttuh028@gmail.com>
2026-03-12 22:00:20 +0530

    Initial Spring Boot setup with Dotenv and Postgres

---
+++ volttrack/src/main/java/com/volttrack/volttrack/VolttrackApplication.java
@@ -0,0 +1,13 @@
+package com.volttrack.volttrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolttrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolttrackApplication.class, args);
+	}
+
+}
---
+++ volttrack/src/test/java/com/volttrack/volttrack/VolttrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volttrack.volttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolttrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
