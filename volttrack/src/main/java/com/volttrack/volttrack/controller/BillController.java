2c17dcf94f83da452f18fe65f181953babe9e38f
Mghiremth0281 <muttuh028@gmail.com>
2026-03-15 17:27:17 +0530

    feat(security): implement role-based access in BillController

--- volttrack/src/main/java/com/volttrack/volttrack/controller/BillController.java
+++ volttrack/src/main/java/com/volttrack/volttrack/controller/BillController.java
@@ -1,11 +1,10 @@
 package com.volttrack.volttrack.controller;
 
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.volttrack.volttrack.dto.bill.BillRequestDto;
@@ -23,24 +22,28 @@ public class BillController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('OFFICER')")
     public ResponseEntity<BillResponseDto> createBill(@Valid @RequestBody BillRequestDto requestDto) {
         BillResponseDto saved = billService.createBill(requestDto);
         return ResponseEntity.ok(saved);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN','OFFICER')")
     public ResponseEntity<Page<BillResponseDto>> getAllBills(Pageable pageable) {
         return ResponseEntity.ok(billService.getAllBills(pageable));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<BillResponseDto> getBillById(@PathVariable Long id) {
-        return ResponseEntity.ok(billService.getBillById(id));
+    @GetMapping("/{billId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('OFFICER') or #billId == authentication.principal.id")
+    public ResponseEntity<BillResponseDto> getBillById(@PathVariable Long billId) {
+        return ResponseEntity.ok(billService.getBillById(billId));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBill(@PathVariable Long id) {
-        billService.deleteBill(id);
+    @DeleteMapping("/{billId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteBill(@PathVariable Long billId) {
+        billService.deleteBill(billId);
         return ResponseEntity.noContent().build();
     }
 }
