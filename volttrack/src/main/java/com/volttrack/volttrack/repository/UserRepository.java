c9e8ea93b53d9af5cb6f93107dafcaa7a843bf12
Mghiremth0281 <muttuh028@gmail.com>
2026-03-15 18:09:06 +0530

    fix(user): Updaet in the methods

--- volttrack/src/main/java/com/volttrack/volttrack/repository/UserRepository.java
+++ volttrack/src/main/java/com/volttrack/volttrack/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.volttrack.volttrack.repository;
 
 import com.volttrack.volttrack.entity.Role;
+import com.volttrack.volttrack.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.volttrack.volttrack.entity.User;
 
 import java.util.Optional;
 
@@ -14,11 +13,10 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
-    // ✅ Add this to check if email already exists
+
     boolean existsByEmail(String email);
 
-    // ✅ Add this to fetch consumers
-    P
-    age<User> findByRole(Role role, Pageable pageable);
+    Optional<User> findByEmail(String email);
 
+    Page<User> findByRole(Role role, Pageable pageable);
 }
