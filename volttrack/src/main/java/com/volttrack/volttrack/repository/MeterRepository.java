bbc689db74915905c757666ff5fbb0bf7bbc5d60
Mghiremth0281 <muttuh028@gmail.com>
2026-03-15 18:11:39 +0530

    feat(meter): add two methods

--- volttrack/src/main/java/com/volttrack/volttrack/repository/MeterRepository.java
+++ volttrack/src/main/java/com/volttrack/volttrack/repository/MeterRepository.java
@@ -1,10 +1,17 @@
 package com.volttrack.volttrack.repository;
 
 import com.volttrack.volttrack.entity.Meter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MeterRepository extends JpaRepository<Meter, Long> {
 
+    Optional<Meter> findByUser_Id(Long userId);
+
+    Page<Meter> findByUser_Id(Long userId, Pageable pageable);
 }
