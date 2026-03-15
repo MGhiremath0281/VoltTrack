b1e67f0f9d019ecbf4cfaaf408aef94680a5f1ca
Mghiremth0281 <muttuh028@gmail.com>
2026-03-15 18:07:45 +0530

    feat(user): add two methods

--- volttrack/src/main/java/com/volttrack/volttrack/repository/UserRepository.java
+++ volttrack/src/main/java/com/volttrack/volttrack/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.volttrack.volttrack.repository;
 
+import com.volttrack.volttrack.entity.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +14,11 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+    // ✅ Add this to check if email already exists
+    boolean existsByEmail(String email);
+
+    // ✅ Add this to fetch consumers
+    P
+    age<User> findByRole(Role role, Pageable pageable);
 
 }
