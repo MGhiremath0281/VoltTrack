cc2f8251d789594b212bfcef5a7ef6db15982b4a
Mghiremth0281 <muttuh028@gmail.com>
2026-03-16 15:37:38 +0530

    feat(docs): add Swagger/OpenAPI configuration

---
+++ volttrack/src/main/java/com/volttrack/volttrack/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.volttrack.volttrack.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("VoltTrack API")
+                        .version("1.0.0")
+                        .description("API documentation for VoltTrack Real-time IoT-based smart electricity meter monitoring system ")
+                        .contact(new Contact()
+                                .name("VoltTrack Team")
+                                .email("support@volttrack.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
