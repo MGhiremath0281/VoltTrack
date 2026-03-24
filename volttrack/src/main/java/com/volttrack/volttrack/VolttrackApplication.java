2ec690852b72266f92f5eb5dbb7f29fd10ee8570
Mghiremth0281 <muttuh028@gmail.com>
2026-03-24 15:28:02 +0530

    feat(cache): enable Redis caching and configure application properties

--- volttrack/src/main/java/com/volttrack/volttrack/VolttrackApplication.java
+++ volttrack/src/main/java/com/volttrack/volttrack/VolttrackApplication.java
@@ -2,8 +2,10 @@ package com.volttrack.volttrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class VolttrackApplication {
 
 	public static void main(String[] args) {
