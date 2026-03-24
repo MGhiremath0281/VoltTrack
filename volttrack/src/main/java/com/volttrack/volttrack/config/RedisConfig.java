d2c82c17b888fa374cadf6262ee9b570240cb633
Mghiremth0281 <muttuh028@gmail.com>
2026-03-24 15:46:55 +0530

    feat(cache): add the redis config to the repo

---
+++ volttrack/src/main/java/com/volttrack/volttrack/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.volttrack.volttrack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)); // cache expires in 10 min
+    }
+}
